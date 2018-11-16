0771c02966423871bae3388cc35b05f378d3f75f
zhipeng_Tong <zhipeng_Tong@126.com>
2018-11-16 14:24:10 +0800

    Flyweight

---
+++ design_patterns/src/cn/ccut/facade/Client.java
@@ -0,0 +1,10 @@
+package cn.ccut.facade;
+
+public class Client {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.methodA();
+        facade.methodB();
+        facade.methodC();
+    }
+}
---
+++ design_patterns/src/cn/ccut/facade/ServiceA.java
@@ -0,0 +1,11 @@
+package cn.ccut.facade;
+
+/**
+ * SubSystem
+ * @author zhipeng_Tong
+ */
+public class ServiceA {
+    public void method() {
+        System.out.println("这里是服务A");
+    }
+}
